ec7147c623cb1e838386fbfe51f5c5c5fd01a49f
rpm26 <ross_261195@hotmail.com>
2016-05-01 13:43:27 +0200

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/question',quizController.question);
 router.get('/check', quizController.check);
-
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
 module.exports = router;
