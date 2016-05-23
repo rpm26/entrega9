2ca70ef19771635fc73d82383a932fa7d0105857
rpm26 <rosa.pita.martin@alumnos.upm.es>
2016-05-23 10:17:27 +0200

    Tema 18

--- models/index.js
+++ models/index.js
@@ -24,11 +24,17 @@ var sequelize = new Sequelize(url,
 				              	omitNull: true 
 				              });
 
+
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+// Importar la definicion de la tabla Comments de comment.js
+var Comment = sequelize.import(path.join(__dirname,'comment'));
 
-
+// Relaciones entre modelos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment;
