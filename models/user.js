55b649c0abca7ae7a3f44c5318ecaba778a22329
rpm26 <rosa.pita.martin@alumnos.upm.es>
2016-05-24 23:35:31 +0200

    Tema 21: interfaz

--- models/user.js
+++ models/user.js
@@ -25,6 +25,12 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.BOOLEAN,
             defaultValue: false
         }
+      },
+      { instanceMethods: {
+          verifyPassword: function (password) {
+            return encryptPassword(password, this.salt) === this.password;
+          }
+        }    
       });
 };
 
