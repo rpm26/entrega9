ca696a847b622f3e2279c91c074534b95a4d1e15
rpm26 <rosa.pita.martin@alumnos.upm.es>
2016-05-25 19:52:48 +0200

    Tema23: Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -2,6 +2,17 @@ var models = require('../models');
 var Sequelize = require('sequelize');
 var url = require('url');
 
+
+
+exports.loginRequired = function (req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/session?redir=' + (req.param('redir') || req.url));
+    }
+};
+
+
 /*
  * Autenticar un usuario: Comprueba si el usuario esta registrado en users
  *
