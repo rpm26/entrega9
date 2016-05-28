574bbe5f876260f0065cf44bc7d6637b4b150e25
rpm26 <rosa.pita.martin@alumnos.upm.es>
2016-05-28 23:25:30 +0200

    Tema27: imagenes. Modificaciones formatos

--- models/index.js
+++ models/index.js
@@ -34,6 +34,9 @@ var Comment = sequelize.import(path.join(__dirname,'comment'));
 // Importar la definicion de la tabla Users de user.js
 var User = sequelize.import(path.join(__dirname,'user'));
 
+// Importar la definicion de la tabla Attachments de attachment.js
+var Attachment = sequelize.import(path.join(__dirname,'attachment'));
+
 // Relaciones entre modelos
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
@@ -42,6 +45,11 @@ Quiz.hasMany(Comment);
 User.hasMany(Quiz, {foreignKey: 'AuthorId'});
 Quiz.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'});
 
+// Relacion 1-a-1 ente Quiz y Attachment
+Attachment.belongsTo(Quiz);
+Quiz.hasOne(Attachment);
+
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 exports.Comment = Comment;
 exports.User = User;
+exports.Attachment = Attachment; // exportar definición de tabla Attachments
